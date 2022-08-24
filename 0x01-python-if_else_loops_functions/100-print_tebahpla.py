d79cb3d42e33817affbae0ebd9cce10b3baeffc2
u31999 <u31999@gmail.com>
2022-08-24 10:53:56 -0700

    Give it permission

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for alpha in range(122, 96, -1):
-    if alpha % 2 != 0 :
+    if alpha % 2 != 0:
         print('{:c}'.format(alpha - 32), end='')
     else:
         print('{:c}'.format(alpha), end='')
