3d5ffb52ac769633c7ed6318428a292859c235b4
u31999 <u31999@gmail.com>
2022-08-25 19:57:25 -0700

    Fix pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
 
     new_argv = argv[1:]
-    
     if len(new_argv) != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
@@ -12,7 +11,6 @@ if __name__ == '__main__':
         a = int(new_argv[0])
         b = int(new_argv[2])
         operater = new_argv[1]
-        
         if operater == '+':
             print('{} {} {} = {}'.format(a, operater, b, add(a, b)))
         elif operater == '-':
