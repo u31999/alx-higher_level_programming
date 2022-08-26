f214fce4ced15432812902aa931ad70a99152966
u31999 <u31999@gmail.com>
2022-08-25 20:06:30 -0700

    Fix pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from sys import argv
-    from calculator_1 import add, sub, mul, div
-
-    new_argv = argv[1:]
-    if len(new_argv) != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+def main(argv):
+    argc = len(argv)
+    ops = {
+        '+': calculator_1.add,
+        '-': calculator_1.sub,
+        '*': calculator_1.mul,
+        '/': calculator_1.div,
+    }
+    if argc != 4:
+        print('Usage: {:s} <a> <operator> <b>'.format(argv[0]))
         exit(1)
-    elif len(new_argv) == 3:
-        a = int(new_argv[0])
-        b = int(new_argv[2])
-        operater = new_argv[1]
-        if operater == '+':
-            print('{} {} {} = {}'.format(a, operater, b, add(a, b)))
-        elif operater == '-':
-            print('{} {} {} = {}'.format(a, operater, b, sub(a, b)))
-        elif opreater == '*':
-            print('{} {} {} = {}'.format(a, operater, b, mul(a, b)))
-        elif operater == '/':
-            print('{} {} {} = {}'.format(a, operater, b, div(a, b)))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    if op not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    res = ops[op](a, b)
+    print('{:d} {:s} {:d} = {:d}'.format(a, op, b, res))
+
+
+if __name__ == '__main__':
+    from sys import argv, exit
+    import calculator_1
+    main(argv)
