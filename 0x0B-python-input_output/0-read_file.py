ea4360736121a860d9b4c28d3490789af6a1e486
u31999 <u31999@gmail.com>
2022-09-27 23:52:25 -0700

    Solutions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Read from a file"""
+
+
 def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as f:
-        data = f.read()
-        print(data, end="")
+        print(f.read(), end="")
