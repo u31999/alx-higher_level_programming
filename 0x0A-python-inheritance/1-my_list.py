3774bf4478a217404806558f9b2d4242cddbe3a6
u31999 <u31999@gmail.com>
2022-09-28 16:16:20 -0700

    Fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,8 +5,12 @@ This module implements a custom list object
 
 
 class MyList(list):
-    """Custom List
+    """Subclass of list
     """
+    def __init__(self):
+        """Intilization"""
+        super().__init__()
+        
     def print_sorted(self):
         """
         prints the list, but sorted (ascending sort)
