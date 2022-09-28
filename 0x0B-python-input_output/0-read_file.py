37e697c564fa9c68e8207fec8d580db7f46cdeff
ahmed <u31999@gmail.com>
2022-09-27 18:19:08 -0700

    Solutions

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,6 +9,6 @@ def read_file(filename=""):
         Raises
         Exception: when the file can be opened
     """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="UTF-8") as f:
         read_data = f.read()
         print(read_data, end='')
